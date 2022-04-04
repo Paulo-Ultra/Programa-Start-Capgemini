98a89e3890fe1a2140c4ef39379169cc9660122d
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-04-03 22:51:43 -0300

    Angular - Introdução a Rotas

---
+++ Angular/Introdução a Rotas/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { AuthGuard } from './auth.guard';
+import { Error404Component } from './error404/error404.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'inicio', component: InicioComponent},
+  {path:'sobre', component: SobreComponent, canActivate:[AuthGuard]},
+  {path:'', redirectTo:'inicio', pathMatch:'full'},
+  {path:'**', component: Error404Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
