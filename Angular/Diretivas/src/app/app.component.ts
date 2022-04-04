b602596b907ea1584d7a5e503264a85e1ba97c20
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-04-03 22:43:50 -0300

    Angular - Diretivas

---
+++ Angular/Diretivas/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  //Diretiva NGIF
+
+  exibirTexto:boolean | undefined;
+
+  acaoNgIf(){
+    this.exibirTexto = !this.exibirTexto;
+  }
+
+  //Diretiva NGFOR
+  cursos = ["HTML & CSS", "PHP Básico", "PHP Avançado", "Angular"];
+
+  //Diretiva NGSWITCH
+  curso_selecionado = "Angular";
+
+  //Diretiva NGCLASS
+  ligada:boolean = true;
+
+}
